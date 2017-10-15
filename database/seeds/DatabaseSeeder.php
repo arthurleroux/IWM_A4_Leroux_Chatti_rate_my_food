1688bc5d6e78d5d86010b2255e3ab1c3164a1abd
Arthur Leroux <Arthur@MacBook-Pro-de-Arthur-2.local>
2017-10-15 16:32:40 +0200

    Add pictureSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(RestaurantsTableSeeder::class);
         $this->call(ReviewsTableSeeder::class);
+        $this->call(PicturesTableSeeder::class);
     }
 }
